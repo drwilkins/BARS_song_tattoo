be543de36759b491487359cb7a7a01e1e99367c8
Matt Wilkins <matt@galacticpolymath.com>
2023-07-09 13:58:08 -0500

    adding MP4 to readme?

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # BARS_song_tattoo
 Code for spectrograms for PhD tat. A composite of male barn swallow songs recorded all across the globe and a female barn swallow song recorded in Colorado.
 
-![BARS_concat_song_4_tat_static_specs](CopyOfBW_composite_spectrogram.png)
+Static spectrogram for tattoo design
+![BARS_concat_song_4_tat_static_specs](BW_composite_spectrogram.png)
+
+Dynamic spectrogram video (with sound)
+<div style="display: flex; justify-content: left;margin-right: auto;">
+<video width="100%" height="auto" src="BARS_concat_song_4_tat.mp4"/>
+</div>
--- make spectrogram.R
+++ make spectrogram.R
@@ -6,6 +6,6 @@ tuneR::readWave("BARS_concat_song_4_tat.wav")
 dynaSpec::prep_static_ggspectro(soundFile="BARS_concat_song_4_tat.wav",colPal = c("white","black"),xLim=6.26,crop = 6.26,yLim = c(1.5,7),ampTrans = 20,savePNG = T,specWidth = 13,specHeight = 2,ovlp = 90,colbins=50)
 
 #output more nuanced color spectrogram for dynamic spectrogram
-for_vid <- dynaSpec::prep_static_ggspectro(soundFile="BARS_concat_song_4_tat.wav",xLim=6.26,yLim = c(1.5,7),ampTrans = 4,savePNG = T,specWidth = 13, specHeight = 2,ovlp = 90,colbins=50,crop = 6.26)
+for_vid <- dynaSpec::prep_static_ggspectro(soundFile="BARS_concat_song_4_tat.wav",xLim=6.26,yLim = c(1.5,7),ampTrans = 4,savePNG = T,specWidth = 13, specHeight = 2,ovlp = 90,colbins=50,crop = 6.26,onlyPlotSpec = F,bgFlood = T)
 
 dynaSpec::paged_spectro(for_vid)
